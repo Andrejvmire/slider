076f4016d64897bd60e01a1d3179deba438d917d
Andrey Yatsenko <andrej_vmire@mail.ru>
2020-05-09 16:36:54 +0300

    подключена автозагрузка jquery

--- webpack.common.config.js
+++ webpack.common.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
     entry: [
@@ -58,5 +59,11 @@ module.exports = {
                 ]
             }
         ]
-    }
+    },
+    plugins: [
+        new webpack.ProvidePlugin({
+            $: 'jquery',
+            jQuery: 'jquery'
+        })
+    ]
 }

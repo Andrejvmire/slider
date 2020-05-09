6a7d5c5265a1b9be707300995cf92347b46ac048
Andrey Yatsenko <andrej_vmire@mail.ru>
2020-05-09 23:47:50 +0300

    добавлен jest удален mocha

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -4,4 +4,8 @@ const common = require("./webpack.common.config")
 module.exports = merge(common, {
     mode: "development",
     devtool: "sourceMap",
+    presets: [
+        ['@babel/preset-env', {targets: {node: 'current'}}],
+        '@babel/preset-typescript',
+    ],
 })

673edf79249c12da1c384ca457ae10370cbaebd3
Andrey Yatsenko <andrej_vmire@mail.ru>
2020-05-10 16:19:13 +0300

    исправлен webpack config

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -4,8 +4,4 @@ const common = require("./webpack.common.config")
 module.exports = merge(common, {
     mode: "development",
     devtool: "sourceMap",
-    presets: [
-        ['@babel/preset-env', {targets: {node: 'current'}}],
-        '@babel/preset-typescript',
-    ],
 })

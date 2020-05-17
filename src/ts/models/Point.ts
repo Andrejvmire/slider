3fe4aa469056c95e802a9d385c5f1575135669c0
Яценко Андрей <andrejvmire@gmail.com>
2020-05-17 14:15:03 +0300

    Модель Slider завершена

--- src/ts/models/Point.ts
+++ src/ts/models/Point.ts
@@ -1,26 +1,13 @@
 export default class Point {
-    private readonly _callback: ((oldState: number, newState: number) => boolean) | undefined;
 
-    constructor(private _value: number, callback?: (oldState: number, newState: number) => boolean) {
-        this._callback = callback;
+    constructor(private _point: number) {
     }
 
-    next(): number {
-        return this._value += 1;
+    get value(): number {
+        return this._point;
     }
 
-    prev(): number {
-        return this._value -= 1;
-    }
-
-    value(): number {
-        return this._value;
-    }
-
-    moveTo(position: number): Point {
-        if (typeof this._callback !== "undefined") {
-            this._value = this._callback(this._value, position) ? position : this._value;
-        }
-        return this;
+    set value(newValue: number) {
+        this._point = newValue;
     }
 }
--- src/ts/models/Slider.ts
+++ src/ts/models/Slider.ts
@@ -1,45 +1,93 @@
 import Point from "./Point";
 
-export class Slider {
-    private readonly _points: Point[];
+type returnEmptyObject = {};
+type returnValue = {
+    from: number,
+    to: number,
+    points: number[]
+}
 
-    private checkPointValue(point: number): boolean;
-    private checkPointValue(point: number[]): boolean;
-    private checkPointValue(point: any): any {
-        if (typeof point === "number") {
-            return (this._from < point) && (point < this._to);
-        } else if (typeof point === "object") {
-            return (this._from < point[0]) && (point[0] < point[1]) && (point[1] < this._to);
+export class Slider {
+    private _points: Point[] | undefined;
+
+    private pointInRange(value: number): boolean {
+        if (value >= this._from && value <= this._to) return true;
+        throw new Error(`Point value ${value} out of range: less ${this._from} or more ${this._to}`);
+    }
+
+    constructor(private _from: number, private _to: number, private _step: number = 1) {
+    }
+
+    points(points: [number]): Slider;
+    points(points: [number, number]): Slider;
+    points(points: any[]): Slider {
+        points = (points[0] === points[1]) ? [points[0]] : points;
+        try {
+            this._points = points
+                .filter(
+                    item => this.pointInRange(item)
+                )
+                .map(
+                    item => new Point(item)
+                )
+        } catch (e) {
+            throw new Error(e.message);
+        }
+        return this;
+    }
+
+    values(): returnValue | returnEmptyObject {
+        if (typeof this._points === 'undefined') return {};
+        return {
+            from: this._from,
+            to: this._to,
+            points: this._points
+                .sort((a, b) => a.value - b.value)
+                .map(
+                    item => item.value
+                )
         }
     }
 
-    constructor(private _from: number, private _to: number, points: number[]) {
-        if (!this.checkPointValue(points)) throw new Error("'Points' out of range");
-        let start = _from;
-        points.map(
-            item => {
-                return new Point(item)
-            }
-        )
-    };
-
-    from(): number {
-        return this._from;
-    };
-
-    to(): number {
-        return this._to;
-    };
-
-    points(): Point[] {
-        return this._points;
+    move(to: number): Slider {
+        try {
+            this.pointInRange(to);
+        } catch (e) {
+            return this;
+        }
+        if (typeof this._points === "undefined") this._points = [new Point(to)];
+        this._points
+            .reduce(
+                (prevValue, curValue) => {
+                    return (Math.abs(prevValue.value - to) < Math.abs(curValue.value - to)) ? prevValue : curValue;
+                }
+            )
+            .value = to;
+        return this;
     }
 
-    values(): number[] {
-        return this._points
-            .map(
-                item => item.value()
-            );
+    private from(currentValue: number, to: number): Slider {
+        if (typeof this._points === 'undefined') return this;
+        try {
+            this.pointInRange(currentValue + to)
+        } catch (e) {
+            return this;
+        }
+        this._points
+            .reverse()
+            .map(item => {
+                if (item.value === currentValue) {
+                    item.value += to;
+                }
+            })
+        return this;
     }
 
+    nextPoint(currentValue: number): Slider {
+        return this.from(currentValue, this._step);
+    }
+
+    prevPoint(currentValue: number): Slider {
+        return this.from(currentValue, -this._step);
+    }
 }

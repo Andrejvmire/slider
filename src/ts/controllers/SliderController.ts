6e8bf0a65593ccffdb6057fe6020aef9220bbc7e
Yatsenko Andrey <andrej_vmire@mail.ru>
2020-05-30 15:41:38 +0300

    классы view без реализации

---
+++ src/ts/controllers/SliderController.ts
@@ -0,0 +1,11 @@
+export default class SliderController implements ISubscriber {
+    private _model: IPublisher;
+    private _view: IPublisher;
+
+    constructor(options: OptionsType) {
+
+    }
+
+    update(data: ModelResponseType): void {
+    }
+}
---
+++ src/ts/types/OptionsType.ts
@@ -0,0 +1,1 @@
+type OptionsType = SliderOptionsType & ViewOptionsType;

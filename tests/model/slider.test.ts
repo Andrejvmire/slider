ce8a1f9a4961ca194bc3ec6c3eda672def74ef78
Yatsenko Andrey <andrej_vmire@mail.ru>
2020-06-12 00:05:21 +0300

    поправил тест slider`a

--- tests/model/slider.test.ts
+++ tests/model/slider.test.ts
@@ -1,7 +1,7 @@
 import Slider from '../../src/ts/models/SliderModel';
 
 describe("Модель Slider", function () {
-    const startObj = <SliderOptionsType>{ruler: [0, 120], points: [0, 120]}
+    const startObj: SliderOptionsType = {ruler: [0, 120], points: [0, 120]}
     describe('Обработка некорректных данных', function () {
         it('Будет вызвана ошибка при создании модели (Points за пределами Ruler`а) для 1-ой точки', function () {
             const errorInSlider = () => {
@@ -22,7 +22,7 @@ describe("Модель Slider", function () {
                 (() => {
                     let slider = new Slider(startObj);
                     slider.move(130);
-                    return slider.value
+                    return slider.state
                 })()
             )
                 .toStrictEqual({min: 0, max: 120, points: [0, 120], step: 1})
@@ -31,8 +31,8 @@ describe("Модель Slider", function () {
             expect(
                 (() => {
                     let slider = new Slider(startObj);
-                    slider.next(120);
-                    return slider.value
+                    slider.move(121, 120);
+                    return slider.state
                 })()
             )
                 .toStrictEqual({min: 0, max: 120, points: [0, 120], step: 1})
@@ -41,8 +41,8 @@ describe("Модель Slider", function () {
             expect(
                 (() => {
                     let slider = new Slider(startObj);
-                    slider.prev(0);
-                    return slider.value
+                    slider.move(-1, 0);
+                    return slider.state
                 })()
             )
                 .toStrictEqual({min: 0, max: 120, points: [0, 120], step: 1})
@@ -53,85 +53,85 @@ describe("Модель Slider", function () {
         it('Вернет значения с одной точкой', () => {
             expect(
                 (new Slider(<SliderOptionsType>{ruler: [10, 120], points: 30}))
-                    .value
+                    .state
             )
-                .toStrictEqual({min: 10, max: 120, points: [30], step: 1})
+                .toStrictEqual({ ruler: {min: 10, max: 120}, points: [30]})
         });
         it('вернет все переданные значения', () => {
             expect(
                 (new Slider(<SliderOptionsType>{ruler: [10, 120], points: [30, 60]}))
-                    .value
+                    .state
             )
-                .toStrictEqual({min: 10, max: 120, points: [30, 60], step: 1})
+                .toStrictEqual({ ruler: {min: 10, max: 120}, points: [30, 60]})
         })
         // Удалена функция генерации одной точки вместо 2-х с одинаковым значением
         // it('объеденит две переданные точки с одинаковыми значениями', () => {
         it('Не объеденит две переданные точки с одинаковыми значениями', () => {
             expect(
                 (new Slider(<SliderOptionsType>{ruler: [10, 120], points: [30, 30]}))
-                    .value
+                    .state
             )
-                .toStrictEqual({min: 10, max: 120, points: [30, 30], step: 1})
+                .toStrictEqual({ ruler: {min: 10, max: 120}, points: [30, 30]})
         });
         it('Передвинет ближайшую к переданному значению  точку (30 => 20)', () => {
             expect(
                 (() => {
                     let slider = new Slider(<SliderOptionsType>{ruler: [10, 120], points: [30, 60]});
                     slider.move(20);
-                    return slider.value;
+                    return slider.state;
                 })()
             )
-                .toStrictEqual({min: 10, max: 120, points: [20, 60], step: 1})
+                .toStrictEqual({ ruler: {min: 10, max: 120}, points: [20, 60]})
         });
         it('Передвинет ближайшую к переданному значению  точку (60 => 70)', () => {
             expect(
                 (() => {
                     let slider = new Slider(<SliderOptionsType>{ruler: [10, 120], points: [30, 60]});
                     slider.move(70);
-                    return slider.value;
+                    return slider.state;
                 })()
             )
-                .toStrictEqual({min: 10, max: 120, points: [30, 70], step: 1})
+                .toStrictEqual({ ruler: {min: 10, max: 120}, points: [30, 70]})
         });
         it('Передвинет точку (30) на один шаг (1) вправо', () => {
             expect(
                 (() => {
                     let slider = new Slider(<SliderOptionsType>{ruler: [10, 120], points: [30, 60]});
-                    slider.next(30);
-                    return slider.value;
+                    slider.move(31, 30);
+                    return slider.state;
                 })()
             )
-                .toStrictEqual({min: 10, max: 120, points: [31, 60], step: 1})
+                .toStrictEqual({ ruler: {min: 10, max: 120}, points: [31, 60]})
         });
         it('Передвинет точку (30) на один шаг (1) влево', () => {
             expect(
                 (() => {
                     let slider = new Slider(<SliderOptionsType>{ruler: [10, 120], points: [30, 60]});
-                    slider.prev(30);
-                    return slider.value;
+                    slider.move(29, 30);
+                    return slider.state;
                 })()
             )
-                .toStrictEqual({min: 10, max: 120, points: [29, 60], step: 1})
+                .toStrictEqual({ ruler: {min: 10, max: 120}, points: [29, 60]})
         });
         it('Передвинет точку (30) на один шаг (15) влево', () => {
             expect(
                 (() => {
                     let slider = new Slider(<SliderOptionsType>{ruler: [10, 120], points: [30, 60], step: 15});
-                    slider.prev(30);
-                    return slider.value;
+                    slider.move(29, 30);
+                    return slider.state;
                 })()
             )
-                .toStrictEqual({min: 10, max: 120, points: [15, 60], step: 15})
+                .toStrictEqual({ ruler: {min: 10, max: 120}, points: [15, 60]})
         });
         it('Передвинет точку (30) на один шаг (22) вправо', () => {
             expect(
                 (() => {
-                    let slider = new Slider(<SliderOptionsType>{ruler: [10, 120], points: [30, 60], step: 22});
-                    slider.next(30);
-                    return slider.value;
+                    let slider = new Slider(<SliderOptionsType>{ruler: [10, 120], points: [30, 60]});
+                    slider.move(31, 30);
+                    return slider.state;
                 })()
             )
-                .toStrictEqual({min: 10, max: 120, points: [52, 60], step: 22})
+                .toStrictEqual({ ruler: {min: 10, max: 120}, points: [52, 60]})
         });
     });
     describe('Тест колбэков', () => {
@@ -139,11 +139,11 @@ describe("Модель Slider", function () {
             subscriber = {
                 update: jest.fn(x => x)
             },
-            startObj = {min: 0, max: 120, step: 1}
+            startObj = {ruler: {min: 10, max: 120}}
         slider.attach(subscriber);
         slider.move(20);
-        slider.next(60);
-        slider.prev(20);
+        slider.move(61, 60);
+        slider.move(19, 20);
         slider.detach(subscriber);
         it('Повторная подписка не должна вызвать ошибку', function () {
             expect(() => slider.attach(subscriber))

52ad218046b0ca03579a7878c5b65d23a51db654
Yatsenko Andrey <andrej_vmire@mail.ru>
2020-07-03 16:43:13 +0300

    тесты для SliderView

---
+++ tests/view/slider.test.ts
@@ -0,0 +1,49 @@
+import SliderView from "../../src/ts/views/SliderView";
+import $ from 'jquery';
+
+describe('Тестируем SliderView', function () {
+    let parent = $(document.createElement('div'));
+    let slider: IViewPublisher & IViewSubscriber & ISlider;
+    describe('Создаем класс', function () {
+        beforeEach(() => {
+            slider = new SliderView({
+                points: [34, 45],
+                tooltip: true,
+                orientation: "vertical",
+                ruler: [0, 120],
+            }, parent);
+        });
+        it('должен веруть объект не undefined', function () {
+            expect(slider)
+                .not.toBeUndefined();
+        });
+        it('должен вернуть $instance типа JQuery', function () {
+            expect(slider.$instance)
+                .toBeInstanceOf($);
+        });
+    });
+    describe('Меняем состояние', function () {
+        beforeEach(() => {
+            slider = new SliderView({
+                points: [-3, 77],
+                ruler: [-23, 199],
+                orientation: "vertical"
+            }, parent)
+        });
+        it('Переместит точку', function () {
+            expect(slider.move(-20, -3).state)
+                .toStrictEqual([-20, 77]);
+        });
+    });
+    describe('Тестируем события', function () {
+        beforeEach(() => {
+            slider = new SliderView({
+                points: [56, 78],
+                ruler: [0, 100]
+            }, parent);
+        });
+        it('должен ', function () {
+
+        });
+    });
+});

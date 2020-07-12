faf9b35f625aa825dfd8cd385f7bc8560912d8c4
Yatsenko Andrey <andrej_vmire@mail.ru>
2020-07-12 22:37:31 +0300

    реализован JQuery плагин

--- src/index.ts
+++ src/index.ts
@@ -10,14 +10,19 @@ import Slider from "./ts/controllers/Slider";
             })
         },
         update: function (value: PointsType): JQuery {
-            console.log(value);
             return this.each(() => {
                 this.slider.update(value);
+                return this;
             })
         },
-        value: function (callback: Function): PointsType {
+        value: function (callback: Function): JQuery {
             return this.each(() => {
-                this.slider.subscribe(callback);
+                let subscriber: ISubscriber = {
+                    update(data?: any) {
+                        callback(data);
+                    }
+                }
+                this.slider.attach(subscriber);
                 return this;
             })
         }
@@ -31,7 +36,7 @@ import Slider from "./ts/controllers/Slider";
         let settings = (opt?: MainOptionsType): MainOptionsType => Object.assign(
             {},
             {
-                ruler: [2, 100],
+                ruler: [0, 100],
                 points: 50,
                 step: 1,
                 orientation: "horizontal",
@@ -40,34 +45,17 @@ import Slider from "./ts/controllers/Slider";
             },
             opt
         );
-        console.log(method, options);
         if (typeof method === "undefined") {
             return methods.init.call(this, settings());
         } else if (typeof method === "object") {
             return methods.init.call(this, settings(method))
         } else if (method === "update") {
             return methods.update.call(this, options);
+        } else if (method === "value") {
+            return methods.value.call(this, options);
         }
-        // if (typeof method === "undefined") {
-        //     return methods.init.call(this, settings(options));
-        // } else if (method === "update") {
-        //     return methods.update.call(this, options);
-        // } else if (method === "value") {
-        //     return methods.value.call(this, options);
-        // }
+        return $.error(`Метод ${method} не определен`);
     }
 
     $.fn.simpleSlider = simpleSlider;
 })(JQuery)
-
-let app = $("#app").simpleSlider();
-let slider = $("#slider_2").simpleSlider({
-    points: [55, 80],
-    ruler: [-55, 230],
-    orientation: "vertical",
-    tooltip: true,
-});
-
-app.simpleSlider("update", [88])
-// app.simpleSlider("value", console.log);
-// slider.simpleSlider("value", console.log);

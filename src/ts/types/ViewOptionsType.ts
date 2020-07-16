80c7b39b65d1dbea957d4e710876b03703875b4f
Yatsenko Andrey <andrej_vmire@mail.ru>
2020-07-16 23:54:09 +0300

    добавил scale

--- src/ts/types/ViewOptionsType.ts
+++ src/ts/types/ViewOptionsType.ts
@@ -2,5 +2,6 @@ type ViewOptionsType = {
     orientation?: SliderOrientationType,
     tooltip?: boolean,
     ruler: RulerType,
-    points: PointsType
+    points: PointsType,
+    scale?: boolean | number[],
 }

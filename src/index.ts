23c994c2c31f014135149e12e5ce9814a44abdd8
Yatsenko Andrey <andrej_vmire@mail.ru>
2020-07-19 15:37:42 +0300

    исправлена ошибка со множественной инициализацией

--- src/index.ts
+++ src/index.ts
@@ -2,21 +2,31 @@ import JQuery from 'jquery';
 import Slider from "./ts/controllers/Slider";
 
 (function ($) {
+    const DATA_FIELD = 'simpleSlider';
     const methods = {
         init: function (opts: MainOptionsType): JQuery {
             return this.each(() => {
+                let data = $(this).data(DATA_FIELD);
+                if (data) return this;
                 this.slider = new Slider(opts, this);
+                $(this).data(DATA_FIELD, {
+                    slider: this
+                })
                 return this;
             })
         },
         update: function (value: PointsType): JQuery {
             return this.each(() => {
+                let data = $(this).data(DATA_FIELD);
+                if (!data) return this;
                 this.slider.update(value);
                 return this;
             })
         },
         value: function (callback: Function): JQuery {
             return this.each(() => {
+                let data = $(this).data(DATA_FIELD);
+                if (!data) return this;
                 let subscriber: ISubscriber = {
                     update(data?: any) {
                         callback(data);
@@ -32,7 +42,7 @@ import Slider from "./ts/controllers/Slider";
     function simpleSlider(options: MainOptionsType): JQuery;
     function simpleSlider(method: "value", callback: Function): JQuery;
     function simpleSlider(method: "update", options: PointsType): JQuery;
-    function simpleSlider(method?: any, options?: any, callback?: Function): any {
+    function simpleSlider(method?: any, options?: any): any {
         let settings = (opt?: MainOptionsType): MainOptionsType => Object.assign(
             {},
             {

5f42f31be8aba9a33ce3a05bc690a39851b7e613
Yatsenko Andrey <andrej_vmire@mail.ru>
2020-09-01 00:04:38 +0300

    изменен IValidator

--- src/ts/interfaces/IValidator.ts
+++ src/ts/interfaces/IValidator.ts
@@ -1,4 +1,8 @@
-interface IValidator {
+interface IValidatorConstructor {
     valid: boolean;
     errors: string[];
+}
+
+interface IValidator {
+    callValidator(functionName: string, value: any, condition: any): IValidatorConstructor
 }

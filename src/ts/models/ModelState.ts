38611935a8d58db9f1c6cfa385d8d8d8b7f4d70d
Yatsenko Andrey <andrej_vmire@mail.ru>
2020-09-03 15:31:35 +0300

    реализован ValidDataState

---
+++ src/ts/models/ModelState.ts
@@ -0,0 +1,12 @@
+import ValidDataState from "../abstract/ValidDataState";
+
+export default class ModelState extends ValidDataState {
+    rules(): RuleType[] {
+        return [
+            {
+                "points": ["inRange", {"field": "ruler"}]
+            },
+        ];
+    }
+
+}

e23b43dd5ceac42bfed19846f7993acccc436484
andrey <andrej_vmire@mail.ru>
2022-02-18 13:42:49 +0300

    тест цепочки обязательств

--- tests/model/chainHandler.test.ts
+++ tests/model/chainHandler.test.ts
@@ -1,22 +1,49 @@
+import "reflect-metadata";
 import MinScaleChainHandler from "../../src/ts/model/MinScaleChainHandler";
 import MaxScaleChainHandler from "../../src/ts/model/MaxScaleChainHandler";
 import StepScaleChainHandler from "../../src/ts/model/StepScaleChainHandler";
 import FromScaleChainHandler from "../../src/ts/model/FromScaleChainHandler";
 import ToScaleChainHandler from "../../src/ts/model/ToScaleChainHandler";
+import {container} from "tsyringe";
+import Mocked = jest.Mocked;
 
 describe("Тест цепочки обязательств", function () {
-    const repository = {
+    const mockRepository = {
             set: jest.fn(),
             get: jest.fn(),
             rollback: jest.fn(),
             acceptValues: jest.fn()
         },
-        min = new MinScaleChainHandler(repository),
-        max = new MaxScaleChainHandler(repository),
-        step = new StepScaleChainHandler(repository),
-        from = new FromScaleChainHandler(repository),
-        to = new ToScaleChainHandler(repository);
-    min.setNext(max).setNext(step).setNext(from).setNext(to);
+        mockValidator = {
+            less: jest.fn(),
+            more: jest.fn(),
+            multiple: jest.fn()
+        };
+
+    let min: IChainHandler<TModelOptions>,
+        max: IChainHandler<TModelOptions>,
+        step: IChainHandler<TModelOptions>,
+        from: IChainHandler<TModelOptions>,
+        to: IChainHandler<TModelOptions>,
+        repository: Mocked<IRepository<TModelOptions>>;
+
+    beforeAll(() => {
+        container.reset();
+        container.register("IRepository", {useValue: mockRepository});
+        repository = container.resolve("IRepository");
+        container.register("IValidator", {useValue: mockValidator});
+    });
+
+    it("не вызывает исключения при инициализации", () => {
+        expect(() => {
+            min = container.resolve(MinScaleChainHandler);
+            max = container.resolve(MaxScaleChainHandler);
+            step = container.resolve(StepScaleChainHandler);
+            from = container.resolve(FromScaleChainHandler);
+            to = container.resolve(ToScaleChainHandler);
+            min.setNext(max).setNext(step).setNext(from).setNext(to);
+        }).not.toThrowError();
+    })
 
     it('set должена быть вызвана 3 раза', () => {
         const req = {min: 2, max: 99, step: 3};
